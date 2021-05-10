69fc4eda19f1d06590415db34ebe18e1ef43f015
Mutahir Pirzada <>
2021-05-10 15:38:47 +0500

    new method added.

--- WeatherApp/Utilities/Utility.swift
+++ WeatherApp/Utilities/Utility.swift
@@ -75,5 +75,19 @@ extension UIViewController {
         alertController.addAction(OKAction)
         self.present(alertController, animated: true, completion:nil)
     }
+    
+    func displayAlertMessage(title:String,messageToDisplay: String)
+    {
+        let alertController = UIAlertController(title: title, message: messageToDisplay, preferredStyle: .alert)
+        
+        let OKAction = UIAlertAction(title: "OK", style: .default) { (action:UIAlertAction!) in
+            
+            // Code in this block will trigger when OK button tapped.
+            print("Ok button tapped");
+        }
+        
+        alertController.addAction(OKAction)
+        self.present(alertController, animated: true, completion:nil)
+    }
 
 }
